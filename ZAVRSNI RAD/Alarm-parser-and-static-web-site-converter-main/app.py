145893a474a991261b493b9d8b09e945439b90bb
spajicdominik <dominikspajic7@gmail.com>
2024-08-16 12:41:16 +0200

    Added Watchdog for data files
    Added a Watchdog for Python to track if any data files are added,removed or modified and render them real time based on changes

--- ZAVRSNI RAD/Alarm-parser-and-static-web-site-converter-main/app.py
+++ ZAVRSNI RAD/Alarm-parser-and-static-web-site-converter-main/app.py
@@ -1,8 +1,14 @@
 import os
 import html
 from flask import Flask, render_template, request
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import threading
+import time
 
 app = Flask(__name__)
+
+data_folder = 'data'
 
 def load_alarms(file_paths):
     alarms = []
@@ -33,15 +39,51 @@ def load_alarms(file_paths):
         alarms.append(alarm)
     return alarms
 
+alarms_data = []
+
+def update_alarms_data():
+    global alarms_data
+    file_paths = [os.path.join(data_folder, f) for f in os.listdir(data_folder) if f.endswith('.txt')]
+    alarms_data = load_alarms(file_paths)
+    print(file_paths)
+    
+class Watcher(FileSystemEventHandler):
+    def on_modified(self, event):
+        if event.is_directory:
+            return
+        if event.src_path.endswith('.txt'):
+            update_alarms_data()
+            
+    def on_created(self, event):
+        self.on_modified(event)
+        
+    def on_deleted(self, event):
+        self.on_modified(event)
+        
+def start_observer():
+    observer = Observer()
+    event_handler = Watcher()
+    observer.schedule(event_handler, path=data_folder, recursive=False)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+    
+
 @app.route('/')
 def index():
-    file_paths = ['alarms1.txt', 'alarms2.txt', 'alarms3.txt']
-    alarms = load_alarms(file_paths)
+    global alarms_data
+
+    severity_options = sorted(set(alarm['Internal Severity'] for alarm in alarms_data))
+    severity_counts = {severity: sum(1 for alarm in alarms_data if alarm['Internal Severity'] == severity) for severity in severity_options}
     
-    severity_options = sorted(set(alarm['Internal Severity'] for alarm in alarms))
-    severity_counts = {severity: sum(1 for alarm in alarms if alarm['Internal Severity'] == severity) for severity in severity_options}
-    
-    return render_template('alarms.html', alarms = alarms, severity_options=severity_options, severity_counts=severity_counts)
+    return render_template('alarms.html', alarms = alarms_data, severity_options=severity_options, severity_counts=severity_counts)
 
 if __name__ == '__main__':
+    update_alarms_data()
+    observer_thread = threading.Thread(target=start_observer, daemon=True)
+    observer_thread.start()
     app.run(debug=True)
