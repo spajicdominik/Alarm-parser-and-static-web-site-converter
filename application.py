5cf713bc9e95d30e59eb2894a316ad3148384548
spajicdominik <dominikspajic7@gmail.com>
2024-08-29 11:29:44 +0200

    Added table form of statistics

--- application.py
+++ application.py
@@ -33,39 +33,57 @@ def load_alarms(file_paths):
     return alarms
 
 
-def generate_html(alarms):              #function for generating HTML code based on the input alarms
+def generate_html(alarms):
     severity_options = set(alarm['Internal Severity'] for alarm in alarms)
-    severity_options_html = '\n'.join(f'<option value="{severity}">{severity}</option>' for severity in sorted(severity_options))   #generating option elements for a severity menu
+    severity_options_html = '\n'.join(f'<option value="{severity}">{severity}</option>' for severity in sorted(severity_options))
 
     alarms_html = ''
     for alarm in alarms:
         context = alarm['Context']
-        context_html = f'<a href="#" class="context-link" data-context="{context}" onclick="filterByContext(event)">{context}</a>'      #hyperlink for displaying alarms with corresponding context - !!!WORK IN PROGRESS!!!!
+        context_html = f'<a href="#" class="context-link" data-context="{context}" onclick="filterByContext(event)">{context}</a>'
 
         alarms_html += f'''
-        
         <tr>
             <td>{(alarm["Alarm ID"])}</td>
             <td>{alarm["Alarm Information"]}</td>
             <td>{alarm["Parameters"]}</td>
             <td>{alarm["Internal Severity"]}</td>
-            <td>{alarm["Explanation"]}</td>                     
+            <td>{alarm["Explanation"]}</td>
             <td>{alarm["Long Description"]}</td>
             <td>{alarm["Troubleshooting"]}</td>
             <td>{context_html}</td>
         </tr>
         '''
-        #generating a table row with the alarm specifications for each alarm and adding it to our HTML code
                                      
     total_alarms = len(alarms)
-    severity_counts = {severity: sum(1 for alarm in alarms if alarm['Internal Severity'] == severity) for severity in severity_options}     #total number of alarms and severities 
+    severity_counts = {severity: sum(1 for alarm in alarms if alarm['Internal Severity'] == severity) for severity in severity_options}
 
-    stats_html = f'<p>Total alarms: {total_alarms}</p>'
-    for severity, count in severity_counts.items():         #generating HTML code displaying the number of alarms and number of severities respectively
-        stats_html += f'<p>{severity}: {count}</p>'
+    stats_html = f'''
+    <table class="stats" id="stats-table">
+        <thead>
+            <tr>
+                <th>Severity</th>
+                <th>Count</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Total alarms</td>
+                <td>{total_alarms}</td>
+            </tr>
+    '''
+    for severity, count in severity_counts.items():
+        stats_html += f'''
+            <tr>
+                <td>{severity}</td>
+                <td>{count}</td>
+            </tr>
+        '''
+    stats_html += '''
+        </tbody>
+    </table>
+    '''
 
-    #GENERATING FULL HTML CONTENT
-    
     html_content = f'''
     <!DOCTYPE html>
     <html lang="en">
@@ -111,8 +129,9 @@ def generate_html(alarms):              #function for generating HTML code based
     </html>
     '''
 
-    with open('alarms.html', 'w', encoding='utf-8') as f:       #creating and writing a HTML file
+    with open('alarms.html', 'w', encoding='utf-8') as f:
         f.write(html_content)
+
 
 #Main script
 file_paths = ['alarms1.txt']
