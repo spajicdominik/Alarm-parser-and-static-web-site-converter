f78cda75f787f5d1a1842622e0bfe3291ebe6fc7
spajicdominik <dominikspajic7@gmail.com>
2024-08-29 11:50:54 +0200

    Added table view of statistics

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -86,4 +86,4 @@ if __name__ == '__main__':
     update_alarms_data()
     observer_thread = threading.Thread(target=start_observer, daemon=True)
     observer_thread.start()
-    app.run(debug=True, host='0.0.0.0', port=5050)
+    app.run(debug=True)
